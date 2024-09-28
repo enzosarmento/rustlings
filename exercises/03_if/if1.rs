81c044c12aa99079b9a449d2e3f08d5b9c061727
enzosarmento <enzosarmento91@gmail.com>
2024-09-28 19:36:24 -0300

    if exercises

--- exercises/03_if/if1.rs
+++ exercises/03_if/if1.rs
@@ -4,10 +4,19 @@ fn bigger(a: i32, b: i32) -> i32 {
     // Do not use:
     // - another function call
     // - additional variables
+    if a > b {
+        return a;
+    } else if b > a {
+        return b;
+    }
+    a
 }
 
 fn main() {
     // You can optionally experiment here.
+    println!("{}", bigger(10, 8));
+    println!("{}", bigger(32, 42));
+    println!("{}", bigger(42, 42))
 }
 
 // Don't mind this for now :)
